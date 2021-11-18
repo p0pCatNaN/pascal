a5ea6e5630d8d8ea4b094fa1be77a3952723d96d
p0pCatNaN <94629189+p0pCatNaN@users.noreply.github.com>
2021-11-18 18:32:22 +0300

    Задание 1,2

---
+++ Задание1.pas
@@ -0,0 +1,42 @@
+﻿Program task1_18_11_21;
+var s,slovo: string;
+var mgl: set of char;
+x, i, a, k: integer;
+m:array of string;
+begin
+readln(s);
+s := s+' ';
+
+setlength(m,1);
+slovo := ' ';
+k := 0;
+for i := 1 to length(s) do
+  if s[i]<>' ' then slovo := slovo+s[i]
+  else
+  begin
+    m[k] := slovo;
+    slovo := ' ';
+    k := k+1;
+    setlength(m,k+1);
+  end;
+  
+  
+var charOfWord: set of char; //множество букв слова
+var mask: set of 'a'..'z';
+
+for i := 1 to length(m[0])  do
+  mask := mask + [m[0][i]];
+
+for i := 1 to k-1 do
+  begin
+    charOfWord := [];
+    for var j := 1 to length(m[i]) do
+     charOfWord := charOfWord + [m[i][j]];
+  
+ mask := mask - charOfWord;
+end;
+
+for var q: char := 'a' to 'z' do
+  if (q in mask) then write(q);
+  
+end.
---
+++ Задание2.pas
@@ -0,0 +1,36 @@
+﻿Program task2_18_11_21;
+var s,slovo: string;
+var mgl: set of char;
+x, i, a, k: integer;
+m:array of string;
+begin
+readln(s);
+s := s+' ';
+
+setlength(m,1);
+slovo := ' ';
+k := 0;
+for i := 1 to length(s) do
+  if s[i]<>' ' then slovo := slovo+s[i]
+  else
+  begin
+    m[k] := slovo;
+    slovo := ' ';
+    k := k+1;
+    setlength(m,k+1);
+  end;
+  
+  
+var charOfWord: set of char; //множество букв слова
+var resultM, mask: set of 'a'..'z';
+
+for i := 1 to length(m[0])  do
+  mask := mask + [m[0][i]];
+
+for i := 1 to length(m[k-1]) do
+  resultM := resultM + (mask * [m[k-1][i]]);
+  
+for var q: char := 'a' to 'z' do
+  if (q in resultM) then write(q);
+  
+end.

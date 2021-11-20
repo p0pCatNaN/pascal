71b445df7989825040c84d50ef75d0aa54e8efd2
p0pCatNaN <94629189+p0pCatNaN@users.noreply.github.com>
2021-11-20 21:06:14 +0300

    Add files via upload

---
+++ file_str.pas
@@ -0,0 +1,32 @@
+﻿begin
+  var f: text;
+  var strOfValues: string;
+  var bufer: string; // элементы до пробела
+  var sum: integer;
+
+  
+  assign(f, 'C:/PascalInput/input.txt');
+  reset(f);
+
+  readln(f, strOfValues); // f - файловая переменная, strOfValues - строка данных
+  
+  strOfValues += ' ';
+
+  for var i := 1 to length(strOfValues) do
+    if(strOfValues[i] <> ' ') then
+    begin
+      writeln('START: bufer: ', bufer, ' strOfValues[i]: ', strOfValues[i]);
+      bufer += strOfValues[i];
+      writeln('END: bufer: ', bufer, ' strOfValues[i]: ', strOfValues[i], newline);
+    end
+ 
+    else
+    begin
+      sum += bufer.toInteger();
+      bufer := '';
+    end;
+    
+    
+  write(sum/10);
+  close(f);
+end.

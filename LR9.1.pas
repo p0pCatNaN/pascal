95fff0455bac623dbfa28fedb14634170cf5549f
p0pCatNaN <94629189+p0pCatNaN@users.noreply.github.com>
2021-11-29 11:38:36 +0300

    LR9

---
+++ LR9.1.pas
@@ -0,0 +1,22 @@
+﻿function elmOfSeries(num: integer): real;
+begin
+  Result := exp(num*ln(num)) / ((1 + num)*(1 + num));
+end;
+
+begin
+  var elm1,elm2,res:real;
+  var count:integer;
+  count := 1;
+  elm1 := elmOfSeries(1);
+  res := elm1;
+  
+  while abs(elm1 - elm2) > 0.001 do
+  begin
+    elm2 := elm1;
+    count += 1;
+    elm1 := elmOfSeries(count);
+    res += elm1;
+  end;
+  
+  writeln (res,' ',count);
+end.
